95c7f30157481fb663a7064b3a667b29251b7c8c
Amodh Gunawardana <amodhgunawardana@gmail.com>
2025-11-05 12:48:39 +0530

    Fix typo in Navbar component title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar=()=>{
         <div className="container flex items-center justify-between">
             <a className="text-xl font-bold text-primary flex items-center" href="#home">
                 <span className="relative z-10">
-                    <span className="text-glow text-foreground">Amodh Gunwaradana</span> Portfilio
+                    <span className="text-glow text-foreground">Amodh Gunwaradana</span> Portfolio
                 </span>
             </a>
 
